c924f9b0b6ef9800943c283d9cec1f4a682c5972
i-am-star <adannaya649@gmail.com>
2023-10-07 09:23:15 +0100

    .

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/**
+* main - Entry point
+*
+* Description: What your code does goes here
+*
+* Return: Always 0 (Success) - what your code returns goes here returns 0 or 1
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
