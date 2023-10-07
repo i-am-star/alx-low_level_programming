1ce8e77d6661d17babd3dba18fb1636df376b403
i-am-star <adannaya649@gmail.com>
2023-10-07 15:57:18 +0100

    .

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/**
+* main - Entry point
+*
+* Description: What your code does goes here
+*
+* Return: Always 0 (Success) - what your code returns goes here returns 0 or 1
+*/
+
+int main(void)
+{
+	char alpha_bet = 'a';
+
+	while (alpha_bet <= 'z')
+	{
+		if (alpha_bet != 'e' && alpha_bet != 'q')
+		{
+			putchar(alpha_bet);
+			alpha_bet++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
