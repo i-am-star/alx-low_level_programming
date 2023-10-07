58fd6b869bb5a158aa14718a6012bba6a4a9ec5e
i-am-star <adannaya649@gmail.com>
2023-10-07 09:20:04 +0100

    .

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/**
+* main - Entry point
+*
+* Description: What your code does goes here
+*
+* Return: Always 0 (Success) - what your code returns goes here returns 0 or 1
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
