cea51e1b10b8d7d4d0fb796557d8b8a7b58f00b8
i-am-star <adannaya649@gmail.com>
2023-10-10 20:01:28 +0100

    .

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/**
+* print_alphabet - Entry point
+*
+* Description: A function that prints the lowercase alphabets
+*
+* Return: void
+*/
+
+void print_alphabet(void)
+{
+	char alpha_bet = 'a';
+
+	while (alpha_bet <= 'z')
+	{
+		_putchar(alpha_bet++);
+	}
+	_putchar('\n');
+}
