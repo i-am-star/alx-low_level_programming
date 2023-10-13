bbe28570a9f46b75c70c87a12c34810e80c31bde
i-am-star <adannaya649@gmail.com>
2023-10-13 14:10:43 +0100

    .

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,7 +19,7 @@ void print_to_98(int n)
 		{
 			printf("%d", n);
 
-			if (n < 98)
+			if (n > 98)
 			{
 				printf(", ");
 			}
@@ -30,7 +30,7 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			 printf("%d\n", n);
+			 printf("%d", n);
 
 			 if (n < 98)
 			 {
