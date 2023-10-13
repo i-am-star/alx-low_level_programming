b0d1105072a0b9ab308645d4d1eb1cac9f790ef1
i-am-star <adannaya649@gmail.com>
2023-10-13 12:04:39 +0100

    .

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/**
+* add - Entry point
+*
+* @a: first parameter
+* @b: second parameter
+*
+* Return: a + b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
