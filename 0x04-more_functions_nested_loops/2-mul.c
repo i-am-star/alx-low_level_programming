8bf739a9689bd3e526f05682a4c0c1157926dfe6
i-am-star <adannaya649@gmail.com>
2023-10-15 15:08:08 +0100

    .

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "main.h"
+
+/**
+* mul - Entry point
+*
+* @a: What your code does goes here
+* @b: What your code does goes here
+*
+* Return: Always 0 (Success) - what your code returns goes here returns 0 or 1
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
