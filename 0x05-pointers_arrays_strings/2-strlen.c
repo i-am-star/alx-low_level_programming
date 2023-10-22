6890f863c71e061824ffcee7d5dfe1b809f2bf1b
i-am-star <adannaya649@gmail.com>
2023-10-22 08:04:01 +0100

    .

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* _strlen - Entry point
+*
+* @s: What your code does goes here
+*
+* Return: Always 0 (Success) - what your code returns goes here returns 0 or 1
+*/
+
+int _strlen(char *s)
+{
+	int len = strlen(s);
+	return (len);
+}
