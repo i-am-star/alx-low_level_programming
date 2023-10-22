448fe1bb730450013aff85c30ba8d734c2a2bfba
i-am-star <adannaya649@gmail.com>
2023-10-22 07:28:59 +0100

    .

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* reset_to_98 - Entry point
+*
+* @n: What your code does goes here
+*
+* Return: Always 0 (Success) - what your code returns goes here returns 0 or 1
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
