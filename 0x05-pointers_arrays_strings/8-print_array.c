8318c4d4c04ee2621e63d36b780bbb6b296075a7
i-am-star <adannaya649@gmail.com>
2023-10-23 13:03:31 +0100

    .

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* print_array - Entry point
+*
+* @a: What your code does goes here
+* @n: second parameter goes here
+*
+* Return: Always 0 (Success) - what your code returns goes here returns 0 or 1
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		
+		if (a[i] != n)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
