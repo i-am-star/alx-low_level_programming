c2c90f27953614b553ec06b337d9b78957eb9cc2
i-am-star <adannaya649@gmail.com>
2023-10-23 10:38:39 +0100

    .

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -18,9 +18,13 @@ void rev_string(char *s)
 
 	len = strlen(s);
 
-	for (i = len - 1; i >= 0; i++)
+	for (i = 0; i < len/2; i++)
 	{
-		_putchar(s[i]);
+		char container = s[i];
+
+		s[i] = s[len - 1 - i];
+
+		s[len - 1 - i] = container;
+
 	}
-	_putchar('\n');
 }
