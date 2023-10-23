22fdb1b0d28832afc3a293a53296f3b2a57894e7
i-am-star <adannaya649@gmail.com>
2023-10-23 13:10:12 +0100

    .

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -19,11 +19,9 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		
-		if (a[i] != n)
-		{
+
+		if (a[i] <= n - 1)
 			printf(", ");
-		}
 	}
 	printf("\n");
 }
