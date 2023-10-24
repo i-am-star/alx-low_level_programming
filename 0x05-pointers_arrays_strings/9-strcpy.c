ba204e312caba0c0a14814a36f25a2aba828072e
i-am-star <adannaya649@gmail.com>
2023-10-24 06:51:35 +0100

    .

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+/**
+* _strcpy - Entry point
+*
+* @dest: What your code does goes here
+* @src: second parameter
+*
+* Return: Always 0 (Success) - what your code returns goes here returns 0 or 1
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
