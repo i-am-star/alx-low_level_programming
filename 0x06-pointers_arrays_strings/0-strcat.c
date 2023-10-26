103c25807d03016491f594b427cc622fdaca4b48
i-am-star <adannaya649@gmail.com>
2023-10-26 20:37:36 +0100

    .

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,20 +15,22 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int len = 0;
+	int len1 = 0;
 	int len2 = 0;
+	int i;
 
-	while (dest[len] != '\0')
-	{
-		len++;
-	}
+	while (dest[len1] != '\0')
+		len1++;
 
 	while (src[len2] != '\0')
 	{
-		dest[len] = src[len2];
-		len++;
 		len2++;
 	}
+
+	for (i = 0; i <= len2; i++)
+	{
+		dest[len1 + i] = src[i];
+	}
+
 	return (dest);
 }
-
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* _strncat - Entry point
+*
+* @dest: What your code does goes here
+* @src: second parameter
+* @n: third parameter
+*
+* Return: Always 0 (Success) - what your code returns goes here returns 0 or 1
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	return (0);
+}
