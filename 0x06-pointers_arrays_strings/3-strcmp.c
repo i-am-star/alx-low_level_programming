5ffd840a5ad5b926db058f4622c58599b2a50a7c
i-am-star <adannaya649@gmail.com>
2023-10-27 15:28:35 +0100

    .

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,6 +14,27 @@
 */
 int _strcmp(char *s1, char *s2)
 {
+	int res, i;
 
-	return (s1);
+	int len1 = strlen(s1);
+	int len2 = strlen(s2);
+	res = 0;
+	i = 0;
+
+	while (s1[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			res = s1[i] - s2[i];
+			break;
+		}
+		i++;
+	}
+
+	if (res == 0 && len1 != len2)
+	{
+		res = s2[i] * -1;
+	}
+
+	return (res);
 }
