06223fb67ab38b84143bf1dfc4f388b19875f3c7
i-am-star <adannaya649@gmail.com>
2023-10-27 14:33:35 +0100

    .

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "main.h"
+#include <time.h>
+
+/**
+* _strcmp - Entry point
+*
+* Description: What your code does goes here
+*
+* Return: Always 0 (Success) - what your code returns goes here returns 0 or 1
+*/
+int _strcmp(char *s1, char *s2)
+{
+
+	return (s1);
+}
