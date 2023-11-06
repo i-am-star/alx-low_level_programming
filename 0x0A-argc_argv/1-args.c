fc3dd01b71a101e00823df803921257bc38178ee
i-am-star <adannaya649@gmail.com>
2023-11-06 12:10:28 +0100

    command line arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* @argc: What your code does goes here
+* @argv: ddw
+*
+* Return: Always 0 (Success) - what your code returns goes here returns 0 or 1
+*/
+int main(int argc, char *argv[])
+{
+	argv = argv;
+
+		printf("%d\n", argc - 1);
+
+	return (0);
+}
