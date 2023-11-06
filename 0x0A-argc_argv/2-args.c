32869f6a163038f0e0221428157942ddc722ff48
i-am-star <adannaya649@gmail.com>
2023-11-06 12:21:11 +0100

    command line arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* @argc: What your code does goes here
+* @argv: dkfr
+*
+* Return: Always 0 (Success) - what your code returns goes here returns 0 or 1
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	argc = argc;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s", argv[i]);
+	}
+	printf("\n");
+
+	return (0);
+}
