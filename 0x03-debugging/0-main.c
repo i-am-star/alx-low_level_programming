944c722ba75e8d909ec40a4c119316c6fbf45a2e
i-am-star <adannaya649@gmail.com>
2023-11-17 15:06:39 +0100

    The art of debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+
+	int r_value = positive_or_negative(i);
+
+	return (r_value);
+}
