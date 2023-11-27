53330b0029c4350628968ac3d20d3eabddd085c7
i-am-star <adannaya649@gmail.com>
2023-11-27 21:01:48 +0100

    .

--- 0x0B-malloc_free/print_char.c
+++ 0x0B-malloc_free/print_char.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+#include <string.h>
+#include "main.h"
+#include <time.h>
 /**
 * main - Entry point
 *
@@ -9,16 +11,12 @@
 *
 * Return: Always 0 (Success) - what your code returns goes here returns 0 or 1
 */
-
-int main(void)
+int print_char(va_list val)
 {
-	char alpha_bet = 'z';
+	char str;
 
-	while (alpha_bet >= 'a')
-	{
-		putchar(alpha_bet);
-		alpha_bet--;
-	}
-	putchar('\n');
-	return (0);
+	str = va_arg(val, int);
+	_putchar(str);
+
+	return (i);
 }
