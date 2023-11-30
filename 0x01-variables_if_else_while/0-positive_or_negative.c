f291fe3ac538cbd67b8aac2ca5f9b6ccfd578fd9
i-am-star <adannaya649@gmail.com>
2023-11-30 06:46:38 +0100

    A program that assigns a random number to the variable n and prints if it is positive or negative.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -22,8 +22,8 @@ int main(void)
 	else if (n == 0)
 		printf("%d is zero\n", n);
 
-	else if (n < 0)
-		printf("%d is less than zero\n", n);
+	else
+		printf("%d is negative\n", n);
 
 
 	return (0);
