9cdadb30dac45bdb64f1a420306bb38a2139c542
i-am-star <adannaya649@gmail.com>
2023-11-30 21:51:19 +0100

    gcc A program that prints all the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Description: A program that prints all the numbers of base 16 in lowercase
+*
+* Return: 0
+*/
+int main(void)
+{
+	int hex_num = 0;
+
+	int num = 0;
+
+	while (hex_num < 16)
+	{
+		if(hex_num < 10)
+		{
+			putchar(hex_num + '0');
+		}
+		hex_num++;
+	}
+
+	while (num <= 5)
+	{
+		if(num <= 5)
+		{
+			putchar(num + 'a');
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
