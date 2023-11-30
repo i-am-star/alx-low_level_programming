4b6c72bec5415ba32baefd4784ff11dd5baff726
i-am-star <adannaya649@gmail.com>
2023-11-30 22:06:53 +0100

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 9-print_comb.c -o 9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Description: program that prints all possible combinations of
+* single-digit numbers
+*
+* Return: 0
+*/
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num + '0');
+
+		if (num != 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
