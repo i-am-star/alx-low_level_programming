c523df17d140e383a7123794ef62e377eed43a0b
i-am-star <adannaya649@gmail.com>
2023-12-14 17:33:20 +0100

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: What your code does goes here
+*
+* Return: Always 0 (Success) - what your code returns goes here returns 0 or 1
+*/
+int main(void)
+{
+	int i, n = 50;
+	unsigned long sum = 0, a = 0, b = 1;
+
+	for (i = 0; i <= n; i++)
+	{
+		sum = a + b;
+
+			printf("%lu, ", sum);
+
+		a = b;
+		b = sum;
+	}
+	printf("\n");
+	return (0);
+}
